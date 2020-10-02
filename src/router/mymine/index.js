2b609bc1aef971031e9355c13ab92102eec86a7e
fish-pao <472184203@qq.com>
2020-10-02 11:55:00 +0800

    add createComponents

---
+++ src/router/mymine/index.js
@@ -0,0 +1,4 @@
+export default{
+    path : '/mymine',
+    component : () => import('@/views/Mymine')
+}
---
+++ src/views/Mymine/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div ia="main" >
+        <Header title="我的喵喵"/>
+        <div id="content">
+            <Login />
+        </div>
+        <Tabbar />
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header';
+import Tabbar from '@/components/Tabbar';
+import Login from '@/components/Login';
+export default {
+    name : 'Mymine',
+    components : {
+        Header,
+        Tabbar,
+        Login
+    }
+}
+</script>
